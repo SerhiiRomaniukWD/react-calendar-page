dd1f4827eea76ca54f9922547fb00c3357e1631f
serhii <deepsparkk@gmail.com>
2023-01-27 14:39:41 +0200

    add calendar-items

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { Calendar } from './components/Calendar';
-import { Header } from './components/Header';
+import { DateNavigator } from './components/DateNavigator';
 
 const App: FC = () => {
+  const [date, setDate] = useState(new Date());
+
+  const handleSetDate = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDate(new Date(event.target.value));
+  };
+
+  const increaseMonth = () => {
+    const newDate = new Date(date.setMonth(date.getMonth()+1));
+
+    setDate(newDate);
+  }; 
+
+  const reduceMonth = () => {
+    const newDate = new Date(date.setMonth(date.getMonth()-1));
+
+    setDate(newDate);
+  };
+
   return (
     <div className="main">
       <div className="container">
-        <Header />
+        <DateNavigator 
+          date={date}
+          increaseMonth={increaseMonth}
+          reduceMonth={reduceMonth}
+          handleSetDate={handleSetDate}
+        />
 
-        <Calendar />
+        <Calendar date={date}/>
       </div>
     </div>
   );
